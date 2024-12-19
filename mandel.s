0fe10bf433deb3a56cc3b0f697e506da78d19e7f
Alistair Keiller <alistair@keiller.net>
2024-12-19 02:38:17 +0000

    update

--- mandel.s
+++ mandel.s
@@ -7,11 +7,13 @@ newline: .ascii "\n"
 newline_len = . - newline
 width = 60
 height = 20
+max_iter = 100
 scale_x: .double 0.03
 scale_y: .double 0.1
 offset_x: .double -2.0
 offset_y: .double -1.0
 zero: .double 0.0
+max_magnitude: .double 4.0
 
 .text
 .global _start
@@ -34,6 +36,7 @@ mandelbrot:
     ldr d5, =scale_y
     ldr d6, =offset_x
     ldr d7, =offset_y
+
 mandelbrot_loop:
     // if (x3 >= height) mandelbrot_done();
     cmp x3, height
@@ -52,17 +55,48 @@ mandelbrot_loop:
     fadd d0, d0, d6
     fadd d1, d1, d7
 
-    // d2 = 0.0, d3 = 0.0, x5=0
+    // d2 = 0.0, d3 = 0.0, w5=0
     ldr d2, =zero
     ldr d3, =zero
-    mov x5, #0
+    mov w5, #0
 
-    // x4 += 1
-    add x4, x4, 1
-    b mandelbrot_loop
+mandelbrot_iter:
+    cmp w5, max_iter
+    bge mandelbrot_plot_space
+
+    // (a+b*i)^2=(a^2-b^2)+2*a*b*i
+    // d8 = d2*d2 - d3*d3 + d0
+    fmul d8, d2, d2
+    fmul d9, d3, d3
+    fsub d8, d8, d9
+    fadd d8, d8, d0
+    // d9 = 2*d2*d3 + d1
+    fmul d9, d2, d3
+    fadd d9, d9, d9
+    fadd d9, d9, d1
+
+    // magnitude = d8*d8 + d9*d9
+    fmul d10, d8, d8
+    fmul d11, d9, d9
+    fadd d10, d10, d11
+
+    // if (magnitude > 4.0) { mandelbrot_plot_star(); }
+    ldr d11, =max_magnitude
+    fcmp d10, d11
+    bgt mandelbrot_plot_star
+
+    // d2 = d8, d3 = d9
+    fmov d2, d8
+    fmov d3, d9
+
+    // iter++
+    add w5, w5, 1
+    b mandelbrot_iter
+
 // return;
 mandelbrot_done:
     ret
+
 // x4 = 0; x3++; putchar('\n');
 mandelbrot_next_line:
     mov x4, 0
@@ -74,4 +108,24 @@ mandelbrot_next_line:
     mov     w8, #64
     svc     #0
 
+    b mandelbrot_loop
+
+mandelbrot_plot_star:
+    mov     x0, #1
+    ldr x1, =star
+    ldr x2, =star_len
+    mov     w8, #64
+    svc     #0
+    b mandelbrot_next_pixel
+
+mandelbrot_plot_space:
+    mov     x0, #1
+    ldr x1, =space
+    ldr x2, =space_len
+    mov     w8, #64
+    svc     #0
+
+// x4 += 1
+mandelbrot_next_pixel:
+    add x4, x4, 1
     b mandelbrot_loop
