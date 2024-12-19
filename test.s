cd94e41368d41e90c7612c7bcf0b86d2e443efab
Alistair Keiller <alistair@keiller.net>
2024-12-19 00:58:34 +0000

    add fib

--- test.s
+++ test.s
@@ -3,6 +3,21 @@ msg:
     .ascii  "Hello, ARM64!\n"
 len = . - msg
 
+fib:
+    mov x3, x0
+    mov x0, 0
+    mov x1, 1
+    mov x2, 0
+fib1:
+    cmp x2, x3
+    beq 1f
+    mov x4, x0
+    mov x0, x1
+    add x1, x4, x1
+    add x2, x2, 1
+    b fib1
+1:
+    ret
 
 .text
 .global _start
@@ -14,7 +29,11 @@ _start:
     mov     w8, #64   // syscall: write
     svc     #0        // make syscall
 
-    // exit(0)
+    // fib(10)
+    mov     x0, #10
+    bl      fib
+
+    // _exit(0)
     mov     x0, #0    // status := 0
     mov     w8, #93   // exit is syscall #93
     svc     #0        // make syscall
